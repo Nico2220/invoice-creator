f944439ed1b8dfe0ae55288a3703cff261e98c87
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-05-18 18:56:16 +0300

    first commit

---
+++ src/lambda/hello/hello.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+export const handler: APIGatewayProxyHandlerV2 = async (
+  event: APIGatewayProxyEventV2
+) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "hello inoice -creator", input: event }),
+  };
+};
