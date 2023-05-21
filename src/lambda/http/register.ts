3804b9d5ff24da3ec94a173656c4719d3c562efa
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-05-21 11:11:08 +0300

    feat(signup): add user signup

---
+++ src/lambda/http/register.ts
@@ -0,0 +1,39 @@
+import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
+import * as AWS from "aws-sdk";
+import * as uuid from "uuid";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const usersTbale = process.env.USERS_TABLE;
+
+export const handler: APIGatewayProxyHandlerV2 = async (
+  event: APIGatewayProxyEventV2
+) => {
+  console.log("event:", event);
+  const body = JSON.parse(event.body);
+
+  const id = uuid.v4();
+  const newItem = {
+    id,
+    ...body,
+  };
+
+  try {
+    await docClient
+      .put({
+        TableName: usersTbale,
+        Item: newItem,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ data: newItem }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ Error: "Error creating user" }),
+    };
+  }
+};
