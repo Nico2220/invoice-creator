0b607d016213d41d3263f623c2963cc5853998fb
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-05-29 09:16:03 +0300

    feat(invoice): add s3 bucket

--- src/lambda/sqs/process-invoice.ts
+++ src/lambda/sqs/process-invoice.ts
@@ -1,10 +1,17 @@
 import { SQSEvent, SQSHandler } from "aws-lambda";
 import * as AWS from "aws-sdk";
-import puppeteer from "puppeteer-core";
+import puppeteer, { Page } from "puppeteer-core";
 import chromium from "chrome-aws-lambda";
+import * as uuid from "uuid";
+import { resolve } from "path";
 
 const sqsClient = new AWS.SQS();
+const s3 = new AWS.S3({
+  signatureVersion: "4",
+});
 const queueUrl = "";
+
+const bucketName = process.env.INVOICES_S3_BUCKET;
 
 export const handler: SQSHandler = async (event: SQSEvent) => {
   console.log("Processing  SQS Event...", JSON.stringify(event));
@@ -31,7 +38,7 @@ export async function createAndUploadPdfONS3() {
   console.log("from Screenshot....");
   const path = await chromium.executablePath;
   console.log("path=", path);
-  let url;
+  let screenshot;
   try {
     const browser = await puppeteer.launch({
       args: chromium.args,
@@ -47,11 +54,35 @@ export async function createAndUploadPdfONS3() {
     await page.goto("https://www.google.com/");
 
     const url = await page.$eval("title", (el) => el.textContent);
-
     console.log("title", url);
+
+    screenshot = await page.screenshot({ path: "/tmp/screenshot.png" });
+    const invoiceId = uuid.v4();
+    // const uploadUrl = s3.upload(
+    //   {
+    //     Bucket: bucketName,
+    //     Key: invoiceId,
+    //     Body: screenshot,
+    //   },
+    //   (err, data) => {
+    //     if (err) console.log("Error uploading File:", JSON.stringify(err));
+    //     resolve(data.Key);
+    //   }
+    // );
+
+    console.log("screenshot", screenshot);
+
     await browser.close();
-    return url;
+    return screenshot;
   } catch (err) {
     console.log("Error", JSON.stringify(err));
   }
+}
+
+async function getUploadUrl(invoiceId: string) {
+  return s3.getSignedUrl("putObject", {
+    Bucket: bucketName,
+    Key: invoiceId,
+    Expires: 300,
+  });
 }
